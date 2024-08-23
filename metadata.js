843b02452ca3bb3519b109e88c2709865bc14478
orcunmadran <orcunmadran@gmail.com>
2024-08-23 18:09:34 +0300

    JSON Test

--- metadata.js
+++ metadata.js
@@ -1,21 +1,25 @@
+// Read JSON Data
 fetch('metadata.json')
-  .then(response => {
-    if (!response.ok) {
-      throw new Error('Network response was not ok');
-    }
-    return response.json();
-  })
-  .then(resources => {
-    const resourceBox = document.getElementById('oer');
-    resources.forEach(resource => {
-        const div = document.createElement('div');
-        div.className = "col";
-        const p = document.createElement('p');
-        p.textContent = resource.title;
-        div.appendChild(p);
-        resourceBox.appendChild(div);
-    });
-  })
-  .catch(error => {
-    console.error('There has been a problem with your fetch operation:', error);
-  });
+    .then(response => response.json())
+    .then(jsonData => {
+        const resourceBox = document.getElementById('oer');
+
+        // Create Cards
+        jsonData.forEach(resource => {
+            const card = document.createElement('div');
+            card.className = 'col-md-4 mb-4';
+            card.innerHTML = `
+                        <div class="card">
+                            <div class="card-body">
+                                <h5 class="card-title">${resource.title}</h5>
+                                <p class="card-text">${resource.description}</p>
+                                <a href="#" target="_blank" class="btn btn-outline-secondary">resource detail</a>
+                                <a href=${resource.identifier} target="_blank" class="btn btn-outline-primary">go to resource</a>
+                            </div>
+                        </div>
+                    `;
+            resourceBox.appendChild(card);
+
+        });
+    })
+    .catch(error => console.error('Veri yüklenirken hata oluştu:', error));
