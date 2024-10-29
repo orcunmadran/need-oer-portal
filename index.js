7b0382f86d8811dd255a6ebe41083b19e286d234
orcunmadran <orcunmadran@gmail.com>
2024-10-29 20:19:33 +0300

    Lang filter is added

---
+++ index.js
@@ -0,0 +1,34 @@
+function getResources() {
+    //Selected language
+    const selectedLang = document.querySelector('input[name="resourceLang"]:checked');
+    // Clear Cards
+    oer.innerHTML = "";
+    // Read JSON Data
+    fetch('metadata.json')
+        .then(response => response.json())
+        .then(jsonData => {
+            const resourceBox = document.getElementById('oer');
+            // Create Filter
+            const filteredData = selectedLang.value ? jsonData.filter(resource => resource.Language === selectedLang.value) : jsonData;
+            // Create Cards
+            filteredData.forEach(resource => {
+                const card = document.createElement('div');
+                card.className = 'col-md-4 mb-4';
+                card.innerHTML = `
+                            <div class="card">
+                                <div class="card-body">
+                                    <h5 class="card-title">${resource.Title}</h5>
+                                    <p class="card-text">${resource.Description}</p>
+                                    <a href="#" target="_blank" class="btn btn-outline-secondary">resource detail</a>
+                                    <a href=${resource.Identifier} target="_blank" class="btn btn-outline-primary">go to resource</a>
+                                </div>
+                            </div>
+                        `;
+                resourceBox.appendChild(card);
+            });
+        })
+        .catch(error => console.error('Veri yüklenirken hata oluştu:', error));
+}
+function clearResources(){
+    oer.innerHTML = "";
+}
